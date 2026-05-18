20fb0b83edb109829cccf2e66b354c63ba04530d
qwekertarisonov-eng <qwekertarsionov@gmail.com>
2026-05-18 12:05:41 +0300

    Usage: gendiff [options] <filepath1> <filepath2>

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,6 +8,10 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command');
+  .helpOption('-h, --help', 'display help for command')
+  // Добавляем опцию формата
+  .option('-f, --format [type]', 'output format')
+  // Добавляем два обязательных аргумента (пути к файлам)
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
