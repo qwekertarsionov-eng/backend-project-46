9f606402839130c9c26995ddeb8fd0af250f25ca
qwekertarisonov-eng <qwekertarsionov@gmail.com>
2026-05-20 13:26:12 +0300

    gendiff file1.json file2.json asciinema rec

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### Hexlet tests and linter status:
 
 [![Actions Status](https://github.com/qwekertarsionov-eng/backend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/qwekertarsionov-eng/backend-project-46/actions)
+
+[![asciicast](https://asciinema.org)](https://asciinema.org/a/bmXACnOnovoWEOfc)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
+import _ from 'lodash';
 import { parseFile } from './parsers.js';
 
-const genDiff = (filePath1, filePath2) => {
-  // Читаем и парсим оба файла в объекты
-  const data1 = parseFile(filePath1);
-  const data2 = parseFile(filePath2);
+const buildDiff = (data1, data2) => {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const unionKeys = _.sortBy(_.union(keys1, keys2));
 
-  // На данном этапе просто выводим данные для проверки работы
-  // (В следующих шагах проекта здесь будет логика сравнения)
-  console.log('Data 1:', data1);
-  console.log('Data 2:', data2);
+  return unionKeys.map((key) => {
+    const hasKey1 = Object.hasOwn(data1, key);
+    const hasKey2 = Object.hasOwn(data2, key);
 
-  // Возвращаем строковое представление (пока заглушка)
-  return 'Здесь будет готовый отчет разницы файлов';
+    switch (true) {
+      case (!hasKey1):
+        return { key, type: 'added', value: data2[key] };
+      case (!hasKey2):
+        return { key, type: 'deleted', value: data1[key] };
+      case (data1[key] !== data2[key]):
+        return { key, type: 'changed', oldValue: data1[key], newValue: data2[key] };
+      default:
+        return { key, type: 'unchanged', value: data1[key] };
+    }
+  });
+};
+
+const formatDiff = (diff) => {
+  const lines = diff.flatMap(({ key, type, value, oldValue, newValue }) => {
+    switch (type) {
+      case 'added':
+        return `  + ${key}: ${value}`;
+      case 'deleted':
+        return `  - ${key}: ${value}`;
+      case 'changed':
+        return [
+          `  - ${key}: ${oldValue}`,
+          `  + ${key}: ${newValue}`
+        ];
+      case 'unchanged':
+        return `    ${key}: ${value}`;
+      default:
+        throw new Error(`Unknown type: ${type}`);
+    }
+  });
+
+  return `{\n${lines.join('\n')}\n}`;
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = parseFile(filepath1);
+  const data2 = parseFile(filepath2);
+
+  const diff = buildDiff(data1, data2);
+  return formatDiff(diff);
 };
 
 export default genDiff;
